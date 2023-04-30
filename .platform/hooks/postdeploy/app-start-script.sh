88a9178c6c263e2951c41b83c1dad2fe11d91f30
James <kanjames@uw.edu>
2023-04-30 01:30:03 -0700

    aws eb deploy final init

--- .platform/hooks/postdeploy/app-start-script.sh
+++ .platform/hooks/postdeploy/app-start-script.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-npm install -g yarn
+
+# load environment variables
+export $(cat /opt/elasticbeanstalk/deployment/env | xargs)
+
+# installing yarn on aws linux 2
+curl -o- -L https://yarnpkg.com/install.sh | bash
+source ~/.bashrc
+
+# build the app
 cd /var/app/current/
 yarn
-yarn start &
+
+# no need to start the app, elastic beanstalk will do it for us
