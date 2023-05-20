2cb93cccaa9f22e16b524ac23977e52ed8e1adeb
James <kanjames@uw.edu>
2023-05-20 14:53:46 -0700

    export env vars to bashrc

--- .platform/hooks/postdeploy/app-start-script.sh
+++ .platform/hooks/postdeploy/app-start-script.sh
@@ -2,6 +2,7 @@
 
 # load environment variables
 export $(cat /opt/elasticbeanstalk/deployment/env | xargs)
+echo 'export $(cat /opt/elasticbeanstalk/deployment/env | xargs)' >> ~/.bashrc
 
 # installing yarn on aws linux 2
 curl -o- -L https://yarnpkg.com/install.sh | bash
@@ -10,5 +11,8 @@ source ~/.bashrc
 # build the app
 cd /var/app/current/
 yarn
+
+export $(cat /opt/elasticbeanstalk/deployment/env | xargs)
+echo 'export $(cat /opt/elasticbeanstalk/deployment/env | xargs)' >> ~/.bashrc
 
 # no need to start the app, elastic beanstalk will do it for us
