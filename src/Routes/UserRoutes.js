b73ec25efa81a850f0e12ddc98e63f25c14232d1
James <kanjames@uw.edu>
2023-05-20 01:39:40 -0700

    contraindications update

--- src/Routes/UserRoutes.js
+++ src/Routes/UserRoutes.js
@@ -7,5 +7,6 @@ router.get('/:id', userController.getUserById);
 router.post('/', userController.createUser);
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
+router.get('/info/:id', userController.getUserInfoById);
 
 module.exports = router;
